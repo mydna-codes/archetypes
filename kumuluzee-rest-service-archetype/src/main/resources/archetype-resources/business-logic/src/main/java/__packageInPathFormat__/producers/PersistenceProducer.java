13c75336c5f7413dd5f9b89305249771df202de7
MusicDin <dm6067@student.uni-lj.si>
2020-12-11 17:56:35 +0100

    Added env variables in deployment and fixed EM producer

--- kumuluzee-rest-service-archetype/src/main/resources/archetype-resources/business-logic/src/main/java/__packageInPathFormat__/producers/PersistenceProducer.java
+++ kumuluzee-rest-service-archetype/src/main/resources/archetype-resources/business-logic/src/main/java/__packageInPathFormat__/producers/PersistenceProducer.java
@@ -2,11 +2,11 @@ package ${package}.producers;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@ApplicationScoped
 public class PersistenceProducer {
 
     @PersistenceContext(unitName = "${artifactId}-jpa-unit")
@@ -16,5 +16,10 @@ public class PersistenceProducer {
     @RequestScoped
     public EntityManager getEntityManager() {
         return em;
+    }
+
+    public void disposeEntityManager(@Disposes EntityManager em){
+        if (em.isOpen())
+            em.close();
     }
 }
